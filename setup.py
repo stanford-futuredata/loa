cae9305edc57a9a258f52f071f90cc8e573bff3c
Daniel Kang <daniel.d.kang@gmail.com>
2022-01-29 23:43:52 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name='loa',
+        version='0.0.1',
+        packages=['loa'],
+        author='Daniel Kang',
+        author_email='daniel.d.kang@gmail.com'
+)
+
